a5e13ba754cc62fd4ff9c1e593bb2aa428f2b967
Dawid Płocki <dawid.plocki@gmail.com>
2021-02-24 16:57:23 +0100

    Solution for "Number Sequences"

---
+++ 2021/Number Sequences/solution.js
@@ -0,0 +1,23 @@
+const { unitTest, loadInputFile, toMD5 } = require("../common");
+
+function calculateSum(number) {
+    const digitsCount = number.length;
+
+    return Array.from(number)
+        .filter((d, i, w) => w[(i + 1) % digitsCount] === d)
+        .map(d => parseInt(d, 10))
+        .reduce((a, b) => a + b, 0);
+}
+
+unitTest(calculateSum('1122'), 3);
+unitTest(calculateSum('1111'), 4);
+unitTest(calculateSum('1234'), 0);
+
+console.log(toMD5(
+    loadInputFile('input.txt')
+        .split('\n')
+        .map(l => calculateSum(l))
+        .map(l => l.toString() + '\n')
+        .join('')
+));
+
